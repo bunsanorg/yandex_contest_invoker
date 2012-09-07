a6f6421bcf25c1bfd882b985b2779cc2ae09f745
Filippov Aleksey <sarum9in@gmail.com>
2012-09-07 19:28:22 +0400

    Process::ResourceLimits::hardMemoryLimitBytes field was removed.

--- include/yandex/contest/invoker/process/ResourceLimits.hpp
+++ include/yandex/contest/invoker/process/ResourceLimits.hpp
@@ -14,14 +14,12 @@ namespace yandex{namespace contest{namespace invoker{namespace process
         {
             ar & BOOST_SERIALIZATION_NVP(timeLimitMillis);
             ar & BOOST_SERIALIZATION_NVP(memoryLimitBytes);
-            ar & BOOST_SERIALIZATION_NVP(hardMemoryLimitBytes);
             ar & BOOST_SERIALIZATION_NVP(outputLimitBytes);
             ar & BOOST_SERIALIZATION_NVP(numberOfProcesses);
         }
 
         std::uint64_t timeLimitMillis = 2 * 1000;               // 2 s
         std::uint64_t memoryLimitBytes = 256 * 1024 * 1024;     // 256 MiB
-        std::uint64_t hardMemoryLimitBytes = 512 * 1024 * 1024; // 512 MiB
         std::uint64_t outputLimitBytes = 2 * 1024 * 1024;       // 2 MiB
 
         /// Number of threads (RLIMIT_NPROC) for process real user id.

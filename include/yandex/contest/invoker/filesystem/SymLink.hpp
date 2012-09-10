9bb7f5dab55753ae05b6a205b1552e0fd93b64ac
Filippov Aleksey <sarum9in@gmail.com>
2012-09-10 11:52:02 +0400

    filesystem fix: <boost/serialization/export.hpp> include was removed.

--- include/yandex/contest/invoker/filesystem/SymLink.hpp
+++ include/yandex/contest/invoker/filesystem/SymLink.hpp
@@ -4,7 +4,6 @@
 
 #include <boost/serialization/access.hpp>
 #include <boost/serialization/nvp.hpp>
-#include <boost/serialization/export.hpp>
 #include <boost/filesystem/path.hpp>
 
 namespace yandex{namespace contest{namespace invoker{namespace filesystem

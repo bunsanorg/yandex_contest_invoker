d9b71e49aa9adf70e719a0c777cfa4c62a2f2ac6
Aleksey Filippov <sarum9in@gmail.com>
2015-07-11 00:28:58 +0400

    Fixed test::getContainerConfig().

--- include/yandex/contest/invoker/test/ContainerConfig.hpp
+++ include/yandex/contest/invoker/test/ContainerConfig.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <yandex/contest/invoker/ContainerConfig.hpp>
+#include <yandex/contest/invoker/filesystem/Directory.hpp>
 
 #include <bunsan/test/environment.hpp>
+
+#include <algorithm>
 
 namespace yandex{namespace contest{namespace invoker{namespace test
 {
@@ -10,10 +13,23 @@ namespace yandex{namespace contest{namespace invoker{namespace test
     {
         ContainerConfig config = ContainerConfig::fromEnvironment();
         config.controlProcessConfig.executable = bunsan::test::dir::binary() / "yandex_contest_invoker_ctl";
-        config.lxcConfig.mount->entries->push_back(system::unistd::MountEntry::bindRO(
-            bunsan::test::dir::binary(),
-            bunsan::test::dir::binary()
-        ));
+        // note: regular test setup is: src = dev/yandex.contest/invoker
+        // bin = dev/yandex.contest/invoker/build
+        const auto dev = bunsan::test::dir::source().parent_path().parent_path();
+        // create all parent directories with 555 permissions
+        const std::size_t dirs_begin_pos = config.filesystemConfig.createFiles.size();
+        for (auto i = dev; i != i.root_path(); i = i.parent_path())
+        {
+            filesystem::Directory dir;
+            dir.path = i;
+            dir.mode = 0555;
+            config.filesystemConfig.createFiles.emplace_back(dir);
+        }
+        std::reverse(config.filesystemConfig.createFiles.begin() + dirs_begin_pos,
+                     config.filesystemConfig.createFiles.end());
+        config.lxcConfig.mount->entries->push_back(
+            system::unistd::MountEntry::bindRO(dev, dev)
+        );
         return config;
     }
 }}}}
